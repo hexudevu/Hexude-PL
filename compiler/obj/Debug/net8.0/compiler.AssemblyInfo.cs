efab8020d2e00d13d44ea3f1eef5012c0d4ce69c
Hexu <hexudevu@gmail.com>
2025-03-11 23:20:34 +0300

    Ast + парсер: начало, основные типы AST

--- compiler/obj/Debug/net8.0/compiler.AssemblyInfo.cs
+++ compiler/obj/Debug/net8.0/compiler.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("compiler")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8ed439770f24eccff183f56b6cbfde0dfed7e1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ddbf631319f33ea100b356db7508854f295ed90")]
 [assembly: System.Reflection.AssemblyProductAttribute("compiler")]
 [assembly: System.Reflection.AssemblyTitleAttribute("compiler")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- utils/AstNodes.cs
+++ utils/AstNodes.cs
@@ -1,5 +1,9 @@
 using System;
+using System.Collections.Generic;
 using Utils;
+namespace Utils {
+    public enum UnaryNotation { Prefix, Postfix }
+}
 namespace AstNodes {
     abstract class AstNode {    public abstract override string ToString(); }
     class IntNode : AstNode {
@@ -30,6 +34,47 @@ namespace AstNodes {
         public VariableNode(string name) => Name = name;
         public override string ToString() => Name;
     }
+    class FunctionArgument<Type> : AstNode {
+        public Type Value { get; }
+        public FunctionArgument(Type value) => Value = value;
+        public override string ToString() => Value?.ToString() ?? "null";
+    }
+    class FunctionCallNode : AstNode {
+        public string Name { get; }
+        public AstNode[] Arguments { get; }
+        public FunctionCallNode(string name, AstNode[] arguments) {
+            Name = name;
+            Arguments = arguments;
+        }
+        public override string ToString() {
+            string returning = Name;
+            string argsToString = "";
+            foreach (AstNode arg in Arguments) {
+                argsToString+=arg.ToString()+", ";
+            }
+            argsToString.Substring(0, argsToString.Length-2);
+            returning += $"({argsToString})";
+            return returning;
+        }
+    }
+    //class ConditionNode : AstNode {
+        //public string Condition;
+        //public AstNode Left, Right;
+    //}
+    class IfNode : AstNode {
+        public AstNode Condition;
+        public AstNode ThenBranch;
+        public AstNode ElseBranch;
+        public IfNode(AstNode condition, AstNode thenBranch, AstNode elseBranch = null) 
+        {
+            Condition = condition;
+            ThenBranch = thenBranch;
+            ElseBranch = elseBranch;
+        }
+        public override string ToString() => ElseBranch != null
+            ? $"if ({Condition}) {{ {ThenBranch} }} else {{ {ElseBranch} }}"
+            : $"if ({Condition}) {{ {ThenBranch} }}";
+    }
     class BinaryOpNode : AstNode {
         public string Operator { get; }
         public AstNode Left { get; }
@@ -40,5 +85,23 @@ namespace AstNodes {
             Right = right;
         }
         public override string ToString() => $"({Left} {Operator} {Right})";
+    }
+    class UnaryOpNode : AstNode {
+        public AstNode Operand { get; }
+        public string Operator { get; }
+        public UnaryNotation Notation { get; }
+        public UnaryOpNode(AstNode operand, string op, UnaryNotation notation) {
+            Operand = operand;
+            Operator = op;
+            Notation = notation;
+        } 
+        public override string ToString() {
+            string returning;
+            if (Notation == UnaryNotation.Prefix)
+                returning = Operator + Operand.ToString();
+            else
+                returning = Operand.ToString() + Operator;
+            return returning;
+        }
     }
 }
